cb79ef75fab6bca4c8a1a5fd432565e6c92bb2c4
suga-ming <kby9901@naver.com>
2022-08-24 16:49:07 +0900

    follow

--- 2/follow_button.js
+++ 2/follow_button.js
@@ -1,8 +1,46 @@
 // React Component!
 // Follow <-> following
 function FollowButton() {
-  return React.createElement("div", {}, "Follow");
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: "12px",
+    right: "16px",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold",
+  };
+
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "black",
+    color: "white",
+    cursor: "pointer",
+  };
+
+  const followingBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid #cfd9de",
+    cursor: "pointer",
+  };
+
+  return React.createElement(
+    "div",
+    {
+      onClick: () => setFollowing(!following),
+      style: following ? followingBtnStyle : followBtnStyle,
+    },
+    following ? "following" : "Follow"
+  );
+  console.log(React.createElement);
 }
 
-const domContainer = document.querySelector("#Follow_button_container");
+const domContainer = document.querySelector("#follow_button_container");
 ReactDOM.render(React.createElement(FollowButton), domContainer);
