72dcd735667f6fd7d879c6d18ba749e9460af676
suga-ming <kby9901@naver.com>
2022-08-31 15:20:00 +0900

    State 원시, 참조 타입

--- practice/src/components/Counter.js
+++ practice/src/components/Counter.js
@@ -1,12 +1,51 @@
 import React, { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const operators = ["+", "-", "*"];
+
+  const [info, setInfo] = useState({
+    count: 0,
+    show: true,
+    operator: operators[0],
+  });
+
+  const hide = () => {
+    setInfo({ ...info, show: !info.show });
+  };
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>증가</button>
+      <button
+        onClick={() => {
+          let result;
+          if (info.operator === "+") result = info.count + 1;
+          if (info.operator === "-") result = info.count - 1;
+          if (info.operator === "*") result = info.count * 1;
+          setInfo({ ...info, count: result });
+          // setCount(result);
+        }}
+      >
+        {info.operator}1
+      </button>
+      <button onClick={hide}>Show and Hide</button>
+      <button
+        onClick={() => {
+          const idx = Math.floor(Math.random() * operators.length);
+          setInfo({ ...info, operator: operators[idx] });
+          //   setOperator(operators[idx]);
+        }}
+      >
+        change Operator
+      </button>
+      {/* <button
+        onClick={() => {
+          setInfo({ ...info, show: !info.show });
+        }}
+      >
+        Show and Hide
+      </button> */}
       <br />
-      Counter: {count}
+      {info.show && `Counter: ${info.count}`}
     </div>
   );
 }
