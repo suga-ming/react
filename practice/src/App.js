5511efdd77c113e194bd8cfe200febc74674a10d
suga-ming <kby9901@naver.com>
2022-08-31 16:07:06 +0900

    state 끌어올리기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 // import Counter from "./components/Counter";
-import Counter2 from "./components/Counter2";
+// import Counter2 from "./components/Counter2";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
-      <Counter2 />
+      <Calculator />
     </div>
   );
 }
